657352c45291ce8e79e7959b1b4f2738fb13a32e
Travis Hill <travismhill@gmail.com>
2020-10-25 17:48:56 -0400

    updated cards

--- public/src/pages/Portfolio.js
+++ public/src/pages/Portfolio.js
@@ -1,105 +1,147 @@
-import React from 'react'
+import React from "react";
 
 export default function Portfolio() {
-    return (
-     
+  return (
     <div className="container mt-5">
-    <div className="row">
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="https://github.com/TravisMH/HW3/blob/master/Assets/images/03-javascript-homework-demo.png?raw=true" className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Password Generator</h5>
+              <p className="card-text">
+              A secure password generator capable of generating a 8-128 character password including caps, non caps, numbers, and/or special characters.
+              </p>
+              <a href="https://github.com/TravisMH/HW3" className="btn btn-primary">
+                View Repository
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="https://github.com/TravisMH/Burger-Logger/blob/master/Burger%20logger.png?raw=true" className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Burger Logger</h5>
+              <p className="card-text">
+                Node, handlebars, express app that logs which burgers have been devoured or trashed.
+              </p>
+              <a href="https://github.com/TravisMH/Burger-Logger" className="btn btn-primary">
+                View Repository
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="https://github.com/TravisMH/Good-ReadMe-Generator/blob/main/README%20GENERATOR.png?raw=true" className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Good README Generator</h5>
+              <p className="card-text">
+                A quick and easy README generator for use with projects on github
+              </p>
+              <a href="https://github.com/TravisMH/Good-ReadMe-Generator" className="btn btn-primary">
+                View Repository
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                4
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                5
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Fitness Tracker</h5>
+              <p className="card-text">
+                A fitness tracker app for monitoring your workouts
+              </p>
+              <a href="#" className="btn btn-primary">
+                View Repository
+              </a>
+            </div>
           </div>
         </div>
       </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
+      <div className="row">
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="card mt-2">
+            <img src="..." className="card-img-top" alt="..."></img>
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </p>
+              <a href="#" className="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div className="row">
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div className="row">
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-      <div className="col-md-4">
-        <div className="card mt-2" >
-          <img src="..." className="card-img-top" alt="..."></img>
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            <a href="#" className="btn btn-primary">Go somewhere</a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-    )
+  );
 }
